7bec772af451e927c4270194a63f2b1f90a20dd9
Hyalin <nicolas.tombal01@gmail.com>
2025-05-15 15:20:13 +0200

    Added template for header, footer and the trees

---
+++ woocommerce/emails/email-footer.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Email Footer
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/email-footer.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 9.6.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+</div>
+<!-- Custom Footer Content Start -->
+<tr>
+    <td class="footer-container">
+        <div class="email-signature">
+            <img src="http://carole-la-porte-a-cote.localwp/wp-content/uploads/2025/05/a-bientot-Carole.png"
+                alt="Signature">
+        </div>
+        <table class="footer-info-table">
+            <tr>
+                <td class="footer-info-left">
+                    <img src="http://carole-la-porte-a-cote.localwp/wp-content/uploads/2025/05/icon-park-solid_local.png"
+                        alt="Point de vente" class="footer-info-icon" /></a>
+                    <p class="footer-info-title">Notre point de vente</p>
+                    <u class="footer-info-value">1455 Boulevard Henri Barbusse,
+                        13020 Bierre-l'Étang</u>
+                </td>
+                <td class="footer-info-right">
+                    <img src="http://carole-la-porte-a-cote.localwp/wp-content/uploads/2025/05/mingcute_phone-fill.png"
+                        alt="Point de vente" class="footer-info-icon" /></a>
+                    <p class="footer-info-title">Besoin d'aide ?</p>
+                    <u class="footer-info-value">(+33) 06 56 66 34 56</u>
+                </td>
+            </tr>
+        </table>
+        <div class="social-media-container">
+            <p class="social-media-title">SUIVEZ NOUS SUR :</p>
+            <a href="#" class="social-media-link facebook">
+                <img src="http://carole-la-porte-a-cote.localwp/wp-content/uploads/2025/05/facebook.png" alt="Facebook"
+                    class="facebook-icon">
+            </a>
+            <a href="#" class="social-media-link instagram">
+                <img src="http://carole-la-porte-a-cote.localwp/wp-content/uploads/2025/05/intagram.png" alt="Instagram"
+                    class="instagram-icon">
+            </a>
+            <a href="#" class="social-media-link">
+                <img src="http://carole-la-porte-a-cote.localwp/wp-content/uploads/2025/05/youtube.png" alt="YouTube"
+                    class="youtube-icon">
+            </a>
+        </div>
+        <p class="footer-contact-info">
+            1455 Boulevard Henri Barbusse, 13020 Bierre-l'Étang<br>
+            <a href="#" class="footer-contact-link">conditions générales de
+                vente</a><br><br>
+            Si vous avez besoin d'aide, veuillez contacter le <strong>service après vente</strong><br>
+            au <a href="tel:+33986563456" class="footer-contact-phone">(+33) 09 86 56 34 56</a> ou par mail : <a
+                href="mailto:support@carole.com" class="footer-contact-email">support@carole.com</a>
+        </p>
+    </td>
+</tr>
+<!-- Custom Footer Content End -->
+
+</td>
+</tr>
+</table>
+<!-- End Content -->
+
+</td>
+</tr>
+</table>
+<!-- End Body -->
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</div>
+</td>
+<td>
+    <!-- Deliberately empty to support consistent sizing and layout across multiple email clients. -->
+</td>
+</tr>
+</table>
+</body>
+
+</html>
